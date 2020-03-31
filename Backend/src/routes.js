f075be175e2c8dab010e230ac1434bcdefdefe42
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2020-03-31 10:18:35 -0300

    Correção do arquivo Routes da aplicação.

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,22 +1,20 @@
+//Criação de variáveis para importação dos controllers e módulos.
 const express = require('express');
 const routes = express.Router();
-const crypto = require('crypto'); //criptografia
+const crypto = require('crypto');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
-//const SessionController = require('./controllers/SessionController');
 const SessionController = require('./controllers/SessionController');
 
+//Importação das Routes responsáveis pelo CRUD.
 routes.post('/sessions', SessionController.create);
-
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
-
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
-
 routes.get('/profile', ProfileController.index);
 
-
-module.exports = routes; // Exportar as rotas
+//Exportando as Routes
+module.exports = routes;
